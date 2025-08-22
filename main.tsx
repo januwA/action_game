d372d2032e568ee05a1529bd483d998b528606da
januwA <ajanuw1995@gmail.com>
2025-08-23 07:35:16 +0800

    1

--- main.tsx
+++ main.tsx
@@ -1,10 +1,8 @@
+import { Link, Route } from "react-router-dom";
 import Img1 from "./assets/a.png";
-export const Settings = {
-  app_name: "动作游戏"
-}
 
-export const Main = () => {
-    return <div>
+const Main = () => {
+  return <div>
     <h1>动作游戏</h1>
     <div style={{
       'display': 'flex',
@@ -14,3 +12,16 @@ export const Main = () => {
     </div>
   </div>
 }
+
+export const Settings = {
+  menu_items: [
+    {
+      key: 'action_game',
+      label: <Link to='action_game'>动作游戏</Link>,
+    }
+  ],
+  route: <Route path='action_game'>
+    <Route index element={<Main />} />
+  </Route>
+}
+
